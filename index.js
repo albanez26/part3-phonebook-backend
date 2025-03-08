eb8941c8a4ef87071ac1841a31b07baa0b0707c0
Edwin Albanez <edwinalbanez23@gmail.com>
2025-03-08 14:23:56 -0600

    3.15: phonebook database, step 3

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ app.get('/api/persons/:id', (request, response) =>{
     }else{
         response.status(404).end();
     }
-});
+})
 
 app.delete('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-
-    response.status(204).end();
-});
+    Person.findByIdAndDelete(request.params.id)
+        .then(person => {
+            response.status(204).end()
+        }).catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) =>{
     const body = request.body;
--- models/person.js
+++ models/person.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 
 const url = process.env.MONGODB_URI;
 
-console.log('Connectiong to ', url);
+console.log('Connecting to ', url);
 
 mongoose.connect(url)
     .then(result => 
