06b5f01087f4b7c2465cda35e99495a49e03989a
Edwin Albanez <edwinalbanez23@gmail.com>
2025-03-25 08:01:29 -0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,55 +3,55 @@ import stylisticJs from '@stylistic/eslint-plugin-js'
 import js from '@eslint/js'
 
 export default [
-    js.configs.recommended,
-    {
-        files: ["**/*.js"],
-        languageOptions: {
-            sourceType: "commonjs",
-            globals: {
-                ...globals.node,
-            },
-            ecmaVersion: "latest",
-        },
-        plugins: {
-            '@stylistic/js': stylisticJs
-        },
-        rules: {
-            '@stylistic/js/indent': [
-                'error',
-                2
-            ],
-            '@stylistic/js/linebreak-style': [
-                'error',
-                'windows',
-            ],
-            '@stylistic/js/quotes': [
-                'error',
-                'single'
-            ],
-            '@stylistic/js/semi': [
-                'error',
-                'never'
-            ],
-            'eqeqeq': 'error',
-            'no-trailing-spaces': 'error',
-            'object-curly-spacing': [
-                'error', 'always'
-            ],
-            'arrow-spacing': [
-                'error', { 'before': true, 'after': true },
-            ],
-            'no-console': 'off',
-            "no-unused-vars": ["error", {
-                "vars": "all",
-                "args": "none",
-                "caughtErrors": "all",
-                "ignoreRestSiblings": false,
-                "reportUsedIgnorePattern": false
-            }]
-        },
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
     },
-    {
-        ignores: ["dist/**", "build/**"],
+    plugins: {
+      '@stylistic/js': stylisticJs
     },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'windows',
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
+      "no-unused-vars": ["error", {
+        "vars": "all",
+        "args": "none",
+        "caughtErrors": "all",
+        "ignoreRestSiblings": false,
+        "reportUsedIgnorePattern": false
+      }]
+    },
+  },
+  {
+    ignores: ["dist/**", "build/**"],
+  },
 ]
